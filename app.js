30935d4bd213d0d33a65989d38037f824c6444a1
WRHR <wrhreeves@gmail.com>
2020-07-06 23:15:40 -0600

    Add login function to return user token

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const baseURL = 'http://localhost:3000'
 const userURL = `${baseURL}/users`
 const loginURL = `${baseURL}/login`
 
-const loginButton = document.querySelector('.login')
+const displayLoginButton = document.querySelector('.login')
 const hideButton = document.querySelector('.hide-button')
+const loginForm = document.querySelector('#login-form')
+const loginSubmitButton = document.querySelector('#login-submit')
 
-loginButton.addEventListener('click', displayLoginForm)
+displayLoginButton.addEventListener('click', displayLoginForm)
 hideButton.addEventListener('click', addHiddenClass)
+
+loginForm.addEventListener('submit', login)
 
 function displayLoginForm(){
     const loginCard = document.querySelector('.login-card')
@@ -15,4 +19,28 @@ function displayLoginForm(){
 
 function addHiddenClass(event){
     event.target.parentNode.classList.add('hidden')
+}
+
+function parseJSON(response){
+    return response.json()
+}
+
+function login(event){
+    event.preventDefault()
+
+    const formData = new FormData(loginForm)
+    const username = formData.get('username')
+    const password =  formData.get('password')
+    const userInfo = { username, password }
+
+    fetch(loginURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userInfo)
+    }).then(parseJSON).then(console.log)
+
+    event.target.reset()
+
 }
